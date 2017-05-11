b964786def516505aa083b5f8bcc4e4541b38b2f
Daniel Klaus <daniel.da.klaus@deutschebahn.com>
2017-05-11 09:54:57 +0200

    Fehler in Main-Methode DartDemo gefixt

--- HelloWorldFX/src/application/DartDemo.java
+++ HelloWorldFX/src/application/DartDemo.java
@@ -2,7 +2,7 @@ package application;
 
 public class DartDemo {
 
-	public static void main() {
+	public static void main(String args[]) {
 		
 		Dartscheibe dartscheibe = new Dartscheibe();
 		int[] score;
