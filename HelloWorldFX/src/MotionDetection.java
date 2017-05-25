cc5383822411b20caa4edbe4521838ded37dbf6f
teamdartscheibe <1TeamDartscheibe>
2017-05-25 20:02:21 +0200

    Motion Detection für mehrere Kameras + Kalibrierung

--- HelloWorldFX/src/MotionDetection.java
+++ HelloWorldFX/src/MotionDetection.java
@@ -7,87 +7,86 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgcodecs.Imgcodecs;
+
+import static org.opencv.videoio.Videoio.CV_CAP_PROP_FRAME_HEIGHT;
+import static org.opencv.videoio.Videoio.CV_CAP_PROP_FRAME_WIDTH;
 
 public class MotionDetection {
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
-
-    static Mat imag = null;
+    //TODO: Start parallel Motion Detection of both cameras
+    //TODO: Calculate the Angle to the Pixel (Rect.width()/2)
+    //TODO: Calculate the crossing point of the two lines
+    //TODO: Calculate the pixel value for 1700 x 1700 coordinate system
 
     public static void main(String[] args) {
-        JFrame jframe = new JFrame("HUMAN MOTION DETECTOR FPS");
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel vidpanel = new JLabel();
-        jframe.setContentPane(vidpanel);
-        jframe.setSize(640, 480);
-        jframe.setVisible(true);
+        MotionDetection motionDetection = new MotionDetection();
+        motionDetection.runDual();
+    }
+    public void runDual(){
+        //Camera Objects
+        //VideoCapture [] videoCaptures = {new VideoCapture(0),new VideoCapture(1)};
+        //SettingObject [] settingObjects = {new SettingObject(), new SettingObject()};
 
-        Mat frame = new Mat();
-        Mat outerBox = new Mat();
-        Mat diff_frame = null;
-        Mat tempon_frame = null;
-        ArrayList<Rect> array = new ArrayList<Rect>();
-        VideoCapture camera = new VideoCapture(0);
-        Size sz = new Size(640, 480);
-        int i = 0;
+        VideoCapture [] videoCaptures = {new VideoCapture(0)};
+        SettingObject [] settingObjects = {new SettingObject()};
+
 
         while (true) {
-            if (camera.read(frame)) {
-                Imgproc.resize(frame, frame, sz);
-                imag = frame.clone();
-                outerBox = new Mat(frame.size(), CvType.CV_8UC1);
-                Imgproc.cvtColor(frame, outerBox, Imgproc.COLOR_BGR2GRAY);
-                Imgproc.GaussianBlur(outerBox, outerBox, new Size(3, 3), 0);
+            for (int j = 0; j < videoCaptures.length; j++){
+                videoCaptures[j].set(CV_CAP_PROP_FRAME_WIDTH,1280);
+                videoCaptures[j].set(CV_CAP_PROP_FRAME_HEIGHT,720);
+                SettingObject s = settingObjects[j];
+                if (videoCaptures[j].read(s.frame)) {
+                    Imgproc.resize(s.frame, s.frame, s.sz);
+                    s.mat = s.frame.clone();
+                    s.outerBox = new Mat(s.frame.size(), CvType.CV_8UC1);
+                    Imgproc.cvtColor(s.frame, s.outerBox, Imgproc.COLOR_BGR2GRAY);
+                    Imgproc.GaussianBlur(s.outerBox, s.outerBox, new Size(3, 3), 0);
 
-                if (i == 0) {
-                    jframe.setSize(frame.width(), frame.height());
-                    diff_frame = new Mat(outerBox.size(), CvType.CV_8UC1);
-                    tempon_frame = new Mat(outerBox.size(), CvType.CV_8UC1);
-                    diff_frame = outerBox.clone();
-                }
-
-                if (i == 1) {
-                    Core.subtract(outerBox, tempon_frame, diff_frame);
-                    Imgproc.adaptiveThreshold(diff_frame, diff_frame, 255,
-                            Imgproc.ADAPTIVE_THRESH_MEAN_C,
-                            Imgproc.THRESH_BINARY_INV, 5, 2);
-                    array = detection_contours(diff_frame);
-                    if (array.size() > 0) {
-                        System.out.println("Motion Detected!");
-
-                        Iterator<Rect> it2 = array.iterator();
-                        while (it2.hasNext()) {
-                            Rect obj = it2.next();
-                            Imgproc.rectangle(imag, obj.br(), obj.tl(),
-                                    new Scalar(0, 255, 0), 1);
-                        }
-
+                    if (s.i == 0) {
+                        s.jFrame.setSize(s.frame.width(), s.frame.height());
+                        s.tempon_frame = new Mat(s.outerBox.size(), CvType.CV_8UC1);
+                        s.diff_frame = s.outerBox.clone();
                     }
+
+                    if (s.i == 1) {
+                        Core.subtract(s.outerBox, s.tempon_frame, s.diff_frame);
+                        Imgproc.adaptiveThreshold(s.diff_frame, s.diff_frame, 255,
+                                Imgproc.ADAPTIVE_THRESH_MEAN_C,
+                                Imgproc.THRESH_BINARY_INV, 5, 2);
+                        s.array = detection_contours(s.mat,s.diff_frame);
+                        if (s.array.size() > 0) {
+                            System.out.println("Motion Detected!");
+
+                            Iterator<Rect> it2 = s.array.iterator();
+                            while (it2.hasNext()) {
+                                Rect obj = it2.next();
+                                //Hier werden die umrandenden Rechtecke eingezeichnet
+                                Imgproc.rectangle(s.mat, obj.br(), obj.tl(),
+                                        new Scalar(0, 255, 0), 1);
+                            }
+                        }
+                        Imgproc.rectangle(s.mat, new Point(35,200), new Point(1225,300),
+                                new Scalar(255, 255, 255), 1);
+                        Imgproc.line(s.mat,new Point(630,200), new Point(630,300),
+                                new Scalar(0,255,255));
+                    }
+
+                    s.i = 1;
+
+                    ImageIcon image = new ImageIcon(Mat2bufferedImage(s.mat));
+                    s.jLabel.setIcon(image);
+                    s.jLabel.repaint();
+                    s.tempon_frame = s.outerBox.clone();
                 }
-
-                i = 1;
-
-                ImageIcon image = new ImageIcon(Mat2bufferedImage(imag));
-                vidpanel.setIcon(image);
-                vidpanel.repaint();
-                tempon_frame = outerBox.clone();
-
             }
         }
     }
@@ -107,7 +106,7 @@ public class MotionDetection {
         return img;
     }
 
-    public static ArrayList<Rect> detection_contours(Mat outmat) {
+    public ArrayList<Rect> detection_contours(Mat imag, Mat outmat) {
         Mat v = new Mat();
         Mat vv = outmat.clone();
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
@@ -119,19 +118,51 @@ public class MotionDetection {
         Rect r = null;
         ArrayList<Rect> rect_array = new ArrayList<Rect>();
 
-        for (int idx = 0; idx < contours.size(); idx++) { Mat contour = contours.get(idx); double contourarea = Imgproc.contourArea(contour); if (contourarea > maxArea) {
-            // maxArea = contourarea;
-            maxAreaIdx = idx;
-            r = Imgproc.boundingRect(contours.get(maxAreaIdx));
-            rect_array.add(r);
-            Imgproc.drawContours(imag, contours, maxAreaIdx, new Scalar(0,0, 255));
-        }
+        for (int idx = 0; idx < contours.size(); idx++) {
+            Mat contour = contours.get(idx);
+            double contourarea = Imgproc.contourArea(contour);
+            if (contourarea > maxArea) {
+                // maxArea = contourarea;
+                maxAreaIdx = idx;
+                r = Imgproc.boundingRect(contours.get(maxAreaIdx));
+                rect_array.add(r);
+                //Hier werden die Konturen vom erkannten Objekt eingezeichnet
+                Imgproc.drawContours(imag, contours, maxAreaIdx, new Scalar(0,255, 255));
+            }
 
         }
 
         v.release();
 
         return rect_array;
+    }
+    class SettingObject {
+        Mat mat, frame, outerBox, diff_frame, tempon_frame;
+        ArrayList<Rect> array;
+        Size sz;
+        int i;
+        JFrame jFrame;
+        JLabel jLabel;
 
+        public SettingObject() {
+            sz = new Size(1280, 720);
+            i = 0;
+            initializeFrame();
+            initializeMat();
+        }
+        void initializeMat(){
+            mat = null;
+            frame = new Mat();
+            diff_frame = null;
+            tempon_frame = null;
+        }
+        void initializeFrame() {
+            jFrame = new JFrame("HUMAN MOTION DETECTOR FPS");
+            jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            jLabel = new JLabel();
+            jFrame.setContentPane(jLabel);
+            jFrame.setSize(1280, 720);
+            jFrame.setVisible(true);
+        }
     }
 }
